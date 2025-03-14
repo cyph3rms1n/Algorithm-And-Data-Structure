e885cd650f0e6041bb22b909a8f076e2f040e2e4
Jyotish yadav <22bcs12392@cuchd.in>
2025-03-14 23:01:53 +0530

    Create LC_1171.cpp

---
+++ Linked List/LC_1171.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        int prefixSum = 0;
+        ListNode* dummyNode = new ListNode(0);
+        dummyNode->next = head;
+        unordered_map<int, ListNode*> mp;
+        mp[0] = dummyNode;
+
+        while(head) {
+            prefixSum += head->val;
+            if (mp.find(prefixSum) != mp.end()) {
+                ListNode* start = mp[prefixSum];
+                ListNode* temp = start;
+
+                int pSum = prefixSum;
+                while (temp != head) {
+                    temp = temp->next;
+                    pSum += temp->val;
+
+                    if (temp != head) {
+                        mp.erase(pSum);
+                    }
+                    start->next = head->next;
+                }
+            } else {
+                mp[prefixSum] = head;
+            }
+            head = head -> next;
+        }
+        return dummyNode ->next;
+    }
+};
+///////////////////////////////////////////////////////////////////////
