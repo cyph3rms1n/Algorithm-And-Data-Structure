a10619aca4ad32c800f5a6da15091041a536337d
Jyotish yadav <22bcs12392@cuchd.in>
2025-03-15 21:23:59 +0530

    Update LC_2130.cpp

--- Linked List/LC_2130.cpp
+++ Linked List/LC_2130.cpp
@@ -12,8 +12,8 @@
 // 1st Approach: Using Vector
 class Solution {
 public:
-    vector<int> temp_arr;
     int pairSum(ListNode* head) {
+        vector<int> temp_arr;
         ListNode* temp = head;
         while (temp) {
             temp_arr.push_back(temp->val);
@@ -34,4 +34,65 @@ public:
     }
 };
 //////////////////////////////////////////////////////////////////
-// 2nd Approach:
+// 2nd Approach: Using Stack
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        stack<int> st;
+        ListNode* temp = head;
+        while (temp) {
+            st.push(temp->val);
+            temp = temp->next;
+        }
+
+        int n = st.size();
+        temp = head;
+        int result = 0;
+        int count = 1;
+        while (count <= n / 2) {
+            result = max(result, st.top() + temp->val);
+            temp = temp->next;
+            st.pop();
+            count++;
+        }
+        return result;
+    }
+};
+//////////////////////////////////////////////////////////////////////
+// 3rd Approach: Reversing the 2nd half of the linked list.
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        // 1st Find Mid
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* mid = slow;
+
+        // 2nd Reverse 2nd half of the Linked List
+        ListNode* prev = nullptr;
+        ListNode* nextNode = nullptr;
+
+        while (mid) {
+            nextNode = mid->next;
+            mid->next = prev;
+            prev = mid;
+            mid = nextNode;
+        }
+
+        // 3. Find Max result
+        int result = 0;
+        ListNode* curr = head;
+
+        while (prev) {
+            result = max(result, curr->val + prev->val);
+            curr = curr->next;
+            prev = prev->next;
+        }
+        return result;
+    }
+};
+////////////////////////////////////////////////////////////////////////
