b0779f22a81b675231f2cea21d5a3a4749879321
Jyotish yadav <22bcs12392@cuchd.in>
2025-03-18 22:15:07 +0530

    Update gfg_cycleDetection.cpp

--- Graph/gfg_cycleDetection.cpp
+++ Graph/gfg_cycleDetection.cpp
@@ -1,6 +1,6 @@
 //geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
-// undirected graph
-// dfs
+//// undirected graph
+////// dfs
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
@@ -24,6 +24,48 @@ class Solution {
         for(int i = 0; i< n;i++){
             if(!visited[i]){
                 if(dfs(adj, i, visited, -1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+////// bfs
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool bfs(vector<vector<int>>& adj, int u, vector<bool>& visited){
+        queue<pair<int,int>> que;
+        que.push({u, -1});
+        visited[u] = true;
+        
+        while(!que.empty()){
+            pair<int,int> temp = que.front();
+            que.pop();
+            
+            int source = temp.first;
+            int parent = temp.second;
+            
+            for(auto& v: adj[source]){
+                if(!visited[v]){
+                    visited[v] = true;
+                    que.push({v,source});
+                }else if(v != parent){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    bool isCycle(vector<vector<int>>& adj) {
+        // Code here
+        int n = adj.size();
+        vector<bool> visited(n,false);
+        
+        for(int i = 0; i< n;i++){
+            if(!visited[i]){
+                if(bfs(adj, i, visited)){
                     return true;
                 }
             }
