239bb07b5688fa2c5e5a2ca4d9f748343f8fa417
Jyotish yadav <22bcs12392@cuchd.in>
2025-03-27 22:55:47 +0530

    Update LC_547.cpp

--- Graph/LC_547.cpp
+++ Graph/LC_547.cpp
@@ -1,4 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////////
+//// Using Extra Space for adjacency list of the grapgh
 class Solution {
 public:
     int n;
@@ -35,3 +36,30 @@ public:
     }
 };
 //////////////////////////////////////////////////////////////////////////////////////
+//// Without using adjacency list
+class Solution {
+public:
+    int n;
+    void DFS(vector<vector<int>>& isConnected, int u, vector<bool>& visited) {
+        visited[u] = true;
+        for (int v = 0; v < n; v++) {
+            if (!visited[v] && isConnected[u][v] == 1) {
+                DFS(isConnected, v, visited);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int count = 0;
+        n = isConnected.size();
+        vector<bool> visited(n, false);
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                DFS(isConnected, i, visited);
+                count++;
+            }
+        }
+        return count;
+    }
+};
+/////////////////////////////////////////////////////////////////////////////////////
