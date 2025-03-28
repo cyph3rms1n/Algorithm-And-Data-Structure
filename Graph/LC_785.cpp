b535ccb42f6b04bea3e06cd63b2b28b57e835ff1
Jyotish yadav <22bcs12392@cuchd.in>
2025-03-29 00:11:27 +0530

    Create LC_785.cpp

---
+++ Graph/LC_785.cpp
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    bool checkBipartiteDFS(vector<vector<int>>& graph, int curr, vector<int>& color, int currColor) {
+        color[curr] = currColor; // color the current node
+
+        // visiting the adjacent node
+        for (auto& v : graph[curr]) {
+
+            if (color[v] == color[curr])
+                return false;
+
+            if (color[v] == -1) { // node never visited
+                int colorOfv = 1 - currColor;
+                if (checkBipartiteDFS(graph, v, color, colorOfv) == false)
+                    return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int v = graph.size();
+        vector<int> color(v, -1);
+
+        for (int i = 0; i < v; i++) {
+            if (color[i] == -1) {
+                if (checkBipartiteDFS(graph, i, color, 1) == false) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
