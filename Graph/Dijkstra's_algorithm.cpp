82d40c044efede5c60b4f0be6a3ad827a3be26d6
Jyotish yadav <22bcs12392@cuchd.in>
2025-04-01 21:15:01 +0530

    Create Dijkstra's_algorithm.cpp

---
+++ Graph/Dijkstra's_algorithm.cpp
@@ -0,0 +1,35 @@
+// geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
+// using priority queue
+class Solution {
+  public:
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex src.
+    vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int src) {
+        // Code here
+        int v = adj.size();
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> result(v,INT_MAX);
+        
+        result[src] = 0;
+        pq.push({0,src});
+        
+        while(!pq.empty()){
+            int d = pq.top().first;
+            int node = pq.top().second;
+            
+            pq.pop();
+            
+            for(auto& vec: adj[node]){
+                int adjNode = vec.first;
+                int weight = vec.second;
+                
+                if((d+weight) < result[adjNode]){
+                    result[adjNode] = d + weight;
+                    pq.push({d+weight, adjNode});
+                }
+            }
+        }
+        return result;
+    }
+};
