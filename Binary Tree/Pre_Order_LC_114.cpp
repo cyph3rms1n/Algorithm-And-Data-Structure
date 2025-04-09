13f1bc24652250ca52d5f12dadb5a79f8b5487ea
Jyotish yadav <22bcs12392@cuchd.in>
2025-04-09 23:05:38 +0530

    Update Pre_Order_LC_114.cpp

--- Binary Tree/Pre_Order_LC_114.cpp
+++ Binary Tree/Pre_Order_LC_114.cpp
@@ -1,4 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
+// Recursive Approach
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -28,3 +29,46 @@ public:
     }
 };
 ////////////////////////////////////////////////////////////////////////////////
+// Iterative Approach. Using stack
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> result;
+    void preOrder(TreeNode* root) {
+        if (root == NULL)
+            return;
+        stack<TreeNode*> st; // stack of node
+        st.push(root);
+
+        while (!st.empty()) {
+            root = st.top();
+            st.pop();
+            // First push right child
+            // Then push left child
+            // So that we can access the left child first from stack
+            result.push_back(root->val);
+            if (root->right != NULL) {
+                st.push(root->right);
+            }
+            if (root->left != NULL) {
+                st.push(root->left);
+            }
+        }
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        preOrder(root);
+        return result;
+    }
+};
+/////////////////////////////////////////////////////////////////////////////////////////////
