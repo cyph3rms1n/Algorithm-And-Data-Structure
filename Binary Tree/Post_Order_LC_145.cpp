00e538ac330482a644986b8bf916f8762f57346e
Jyotish yadav <22bcs12392@cuchd.in>
2025-04-10 22:03:13 +0530

    Update Post_Order_LC_145.cpp

--- Binary Tree/Post_Order_LC_145.cpp
+++ Binary Tree/Post_Order_LC_145.cpp
@@ -1,4 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////////////
+// Recursive Approach
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -27,3 +28,46 @@ public:
     }
 };
 /////////////////////////////////////////////////////////////////////////////////////////
+// Iterative Approach. Using 2 stack
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> st1, st2;
+        if (root == NULL)
+            return result;
+
+        st1.push(root);
+        while (!st1.empty()) {
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+
+            if (root->left != NULL) {
+                st1.push(root->left);
+            }
+            if (root->right != NULL) {
+                st1.push(root->right);
+            }
+        }
+
+        while (!st2.empty()) {
+            result.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return result;
+    }
+};
+//////////////////////////////////////////////////////////////////////////////////////////////////
