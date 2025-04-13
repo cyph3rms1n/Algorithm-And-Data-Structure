05eeb7b57dae99cc7823ce59f1440b39c5d4d017
Jyotish yadav <22bcs12392@cuchd.in>
2025-04-13 22:55:03 +0530

    Update Right_Side_View_LC_199.cpp

--- Binary Tree/Right_Side_View_LC_199.cpp
+++ Binary Tree/Right_Side_View_LC_199.cpp
@@ -11,6 +11,7 @@
  * right(right) {}
  * };
  */
+//  Using Level Order Traversal
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
@@ -35,6 +36,28 @@ public:
             }
             result.push_back(node->val);
         }
+        return result;
+    }
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Using DFS
+class Solution {
+public:
+    void preOrder(TreeNode* root, int level, vector<int>& result) {
+        if (root == NULL)
+            return;
+
+        if (result.size() < level) {
+            result.push_back(root->val);
+        }
+
+        preOrder(root->right, level + 1, result);
+        preOrder(root->left, level + 1, result);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+
+        preOrder(root, 1, result);
         return result;
     }
 };
