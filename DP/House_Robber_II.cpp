65d2aa2317249d012096af858a7d1aad8b2d306a
Jyotish yadav <22bcs12392@cuchd.in>
2025-05-17 09:43:52 +0530

    Update House_Robber_II.cpp

--- DP/House_Robber_II.cpp
+++ DP/House_Robber_II.cpp
@@ -34,3 +34,41 @@ public:
     }
 };
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Approach 2: Bottom - Up
+// TC: o(n) SC: o(n)
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+
+        vector<int> t(n + 1, 0);
+        t[0] = 0;
+
+        for (int i = 1; i <= n - 1; i++) {
+            int steal = nums[i - 1] + ((i - 2 >= 0) ? t[i - 2] : 0);
+            int skip = t[i - 1];
+
+            t[i] = max(steal, skip);
+        }
+        int result1 = t[n - 1];
+
+        t.clear();
+
+        t[0] = 0;
+        t[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            int steal = nums[i - 1] + ((i - 2 >= 0) ? t[i - 2] : 0);
+            int skip = t[i - 1];
+
+            t[i] = max(steal, skip);
+        }
+        int result2 = t[n];
+
+        return max(result1, result2);
+    }
+};
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
