171d603d5a993daff1c0cc3402d7ba10ac47b5a5
Jyotish yadav <22bcs12392@cuchd.in>
2025-05-29 08:49:43 +0530

    Update Longest_Increasing_Subsequence.cpp

--- DP/Longest_Increasing_Subsequence.cpp
+++ DP/Longest_Increasing_Subsequence.cpp
@@ -56,4 +56,24 @@ public:
         return maxU;
     }
 };
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Approach 3: Patience Sorting
+// TC: o(nlogn) SC:o(n)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sorted;
+
+        for (int i = 0; i < n; i++) {
+            auto it = lower_bound(sorted.begin(), sorted.end(), nums[i]);
+            if (it == sorted.end()) {
+                sorted.push_back(nums[i]);
+            } else {
+                *it = nums[i];
+            }
+        }
+        return sorted.size();
+    }
+};
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
